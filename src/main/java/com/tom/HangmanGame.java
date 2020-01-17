51bb3531cdc4fe00cc0ecf404e4c8eb2aeaeb956
Tomek <tomm.wrobel@gmail.com>
2020-01-17 20:58:29 +0100

    Initial commit

---
+++ src/main/java/com/tom/HangmanGame.java
@@ -0,0 +1,12 @@
+package com.tom;
+
+public class HangmanGame {
+    public static void main(String[] args) {
+        System.out.println(" ".isBlank()); //true
+
+        String s = "Anupam";
+        System.out.println(s.isBlank()); //false
+        String s1 = "";
+        System.out.println(s1.isBlank()); //true
+    }
+}
